985a012c4a9e176f271a435903bb3a44074b74ce
Milos Popovic <milos_popovic@outlook.com>
2020-07-21 16:19:50 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,14 @@
-# Make your shoe class here!
 class Shoe
 
-  # def initialize(name)
-  #   @name = name
-  # end
+  def initialize(brand)
+    @brand = brand
+  end
 
+
+  attr_reader :title
+  attr_accessor :author :page_count :genre
+
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
 end
